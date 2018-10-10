d59d73041d516babefacc9ac620fa5074fe555f9
fkkmemi <fkkmemi@gmail.com>
2018-10-10 14:28:09 +0900

    3.3 테스트 완료

--- src/views/test.vue
+++ src/views/test.vue
@@ -3,3 +3,17 @@
     <h1>This is a test page</h1>
   </div>
 </template>
+<script>
+export default {
+  mounted () {
+    const obj = {
+      s: '문자',
+      n: 12,
+      as: ['사과', '바나나']
+    }
+    console.log(obj.s, obj.n, obj.as[0]) // 문자 12 사과
+    console.log(obj['s'], obj['n'], obj['as'][0]) // 문자 12 사과
+
+  }
+}
+</script>
